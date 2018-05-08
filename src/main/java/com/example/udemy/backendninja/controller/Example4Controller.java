09ae85595e95a4edbf3ee7699f2b2e6e25b04422
Bernardo <bhernandez@consistent.com.mx>
2018-05-08 09:15:23 -0500

    manejo de errores

--- src/main/java/com/example/udemy/backendninja/controller/Example4Controller.java
+++ src/main/java/com/example/udemy/backendninja/controller/Example4Controller.java
@@ -16,6 +16,8 @@ public class Example4Controller {
 	
 	public static final String FORM = "formulario";
 	public static final String ADD_AlUMNO = "addalumno";
+	public static final String ERROR = "404";
+	public static final String ERROR2 = "500";
 	@GetMapping("/mostrarformulario")
 	public String mostrarFormulario(Model model){
 		model.addAttribute("alumno", new Alumno());
@@ -26,7 +28,15 @@ public class Example4Controller {
 	public ModelAndView addAlumno(@ModelAttribute("alumno") Alumno alumno){
 		ModelAndView model = new ModelAndView(ADD_AlUMNO);
 		model.addObject("alumno", alumno);
-		return model;
-		
+		return model;	
+	}
+	
+	@GetMapping("/error")
+	public String showError(){
+		return ERROR;
+	}
+	@GetMapping("/error2")
+	public String showError2(){
+		return ERROR2;
 	}
 }
